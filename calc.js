e2cab3aa657fcb4ad1dee93b5a2c4140d8d4be88
Leonardo Marcatti da Silva <leonardomarcatti@hotmail.com>
2020-05-22 21:00:44 -0300

    Updates made

--- calc.js
+++ calc.js
@@ -4,26 +4,58 @@ m2 = 0;
 x = 0;
 y = 0;
 $('#keyboard').children().on('click', function(e){
-        let valor = e.target.innerHTML;
-        let display = $('#display').val();
-        if (!isNaN(valor) || valor == '.') {
-            if (valor == ".") {
-                $('#display'). val( $('#display'). val() + valor);
-                $('#history').val($('#history').val() + valor);
-            } else{
-                $('#display').val($('#display').val() + valor);
-                $('#history').val($('#history').val() + valor);
-            }
-        } else{
-            if (display != '' && valor != '=')  {
-                $('#history').val($('#history').val() + valor);
-                $('#display').val('');
-            } else 
-                if (display != "" && valor == '=') {
-                    $('#display').val(eval($('#history').val()));
-                    $('#history').val($('#history').val() + '=');
-            }
+    $('#igual').removeAttr('disabled');
+    let valor = e.target.innerHTML;
+    let display = $('#display');
+    let history = $('#history');
+        let total;
+    if(history.val() == ''){
+        if(valor == '/' || valor == '*' || valor == '+' || valor == '-' || valor == '=') {
+            false;
+        }else if(valor == '.'){
+             history.val(0 + valor);
+        }else{
+            history.val(history.val() + valor);
         };
+    }else if(history.val() != ''){
+        if(history.val() == '0.' && (valor == '/' || valor == '*' || valor == '-' || valor == '+' || valor == '.')){
+            false;
+        }else if((history.val().substr(-1,1) == '/' || history.val().substr(-1,1) == '*' || history.val().substr(-1,1) == '-' || history.val().substr(-1,1) == '+') && valor == '.'){
+            history.val(history.val() + 0+valor);                
+        }else if ((history.val().substr(-1,1) == '/' || history.val().substr(-1,1) == '*' || history.val().substr(-1,1) == '-' || history.val().substr(-1,1) == '+') && (valor == '/' || valor == '*' || valor == '-' || valor == '+')){
+            false;
+        }else if(history.val().substr(-1,1) == '.' && (valor == '/' || valor == '*' || valor == '-' || valor == '+')){
+            history.val(ReplaceDot(valor));
+        }else if(!history.val().includes('=')){
+            if (valor != '=') {
+                history.val(history.val() + valor);
+            };
+        }else if(valor == '.'){
+            history.val(0 + valor);
+        }else{
+            history.val(valor);
+        };
+    };
+});
+
+function ReplaceDot(param){
+    let v1,v2;
+    v1 = $('#history').val();
+    v2 = v1.replace('.', '');
+    return v2 + param;
+};
+
+$('#ponto').on('click', function(){ $(this).attr('disabled', '');});
+$('#keyboard').children().on('click', (param) => {
+    if(param.target.innerHTML == '/' || param.target.innerHTML == '*' || param.target.innerHTML == '-' || param.target.innerHTML == '+'){$('#ponto').removeAttr('disabled');
+    };
+});
+
+$('#igual').on('click', () => {
+    $('#display').val(eval($('#history').val()));
+    $('#history').val($('#history').val() + '=');
+    $('#igual').attr('disabled', '');
+    $('#ponto').removeAttr('disabled');
 });
 
 $('#c').on('click', () => {    
@@ -31,6 +63,7 @@ $('#c').on('click', () => {
     $('#history').val('');
     $('#xy').css('color', 'black');
     $('sup').css('color', 'black');
+    $('#ponto').removeAttr('disabled');
     x = 0,
     y = 0;
 });
@@ -38,7 +71,7 @@ $('#c').on('click', () => {
 $('#1x').on('click', 
     ()=> $('#display').val(1/$('#display').val()));
 
-$('#m1').on('click', 
+$('#m1').on('click',
     () => {if (m1 != 0) {
         $('#display').val(m1);
         $('#history').val($('#history').val() + $('#display').val());
@@ -49,7 +82,7 @@ $('#m1').on('click',
     };
 });
 
-$('#m2').on('click', 
+$('#m2').on('click',
     () => {if (m2 != 0) {
         $('#display').val(m2);
         $('#history').val($('#history').val() + $('#display').val());
@@ -68,7 +101,7 @@ $('#xy').on('click', () =>{
         $('#display').val('');
         $('#xy').css('color', 'red');
         $('sup').css('color', 'black');
-        $('#history').val(x + '^');    
+        $('#history').val(x + '^');
     } else {
         y = $('#display').val();
         $('sup').css('color', 'red');
